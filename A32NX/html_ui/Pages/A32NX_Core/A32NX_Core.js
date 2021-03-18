5168f890860c12f8b253cb80cc5c581dfece2fba
David Walschots <davidwalschots@users.noreply.github.com>
2021-03-18 16:48:37 +0100

    chore: remove unused ACPowerStateChange code
    The toggling of circuit 79 is a remnant from PR #796. In PR #2119 Harry
    changed circuit 79 to refer to a different element. On requesting
    information Harry indicated not being aware of this code, nor of how to
    test if this code is needed. Therefore I'll simply remove it and we'll
    see what happens...

--- A32NX/html_ui/Pages/A32NX_Core/A32NX_Core.js
+++ A32NX/html_ui/Pages/A32NX_Core/A32NX_Core.js
@@ -19,7 +19,6 @@ class A32NX_Core {
     }
 
     init(startTime) {
-        this.ACPowerStateChange = false;
         this.getDeltaTime = A32NX_Util.createDeltaTimeCalculator(startTime);
         this.modules.forEach(module => {
             if (typeof module.init === "function") {
@@ -34,20 +33,11 @@ class A32NX_Core {
             return;
         }
 
-        this.updateACPowerStateChange();
-
         const deltaTime = this.getDeltaTime();
 
         this.soundManager.update(deltaTime);
         this.modules.forEach(module => {
             module.update(deltaTime, this);
         });
-    }
-    updateACPowerStateChange() {
-        const engineOn = Simplane.getEngineActive(0) || Simplane.getEngineActive(1);
-        const externalPowerOn = SimVar.GetSimVarValue("EXTERNAL POWER AVAILABLE:1", "Bool") === 1 && SimVar.GetSimVarValue("EXTERNAL POWER ON", "Bool") === 1;
-        const apuOn = SimVar.GetSimVarValue("L:APU_GEN_ONLINE", "bool");
-        const isACPowerAvailable = engineOn || apuOn || externalPowerOn;
-        this.ACPowerStateChange = (isACPowerAvailable != this.ACPowerLastState);
     }
 }
